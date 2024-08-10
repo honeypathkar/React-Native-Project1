444724f8ba6b00792f5c49b32763dc35be53bb97
honeypathkar <honeypatkar70@gmail.com>
2024-08-10 14:06:51 +0530

    Task 1 Adding Card

--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -4,11 +4,54 @@ import React from 'react';
 const FlatCards = () => {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>Flat Cards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Blue</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default FlatCards;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingHorizontal: 15,
+    paddingTop: 10,
+    color: "black"
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    padding: 10,
+  },
+  card: {
+    width: 100,
+    height: 100,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    borderRadius: 4,
+  },
+  cardOne: {
+    backgroundColor: 'red',
+  },
+  cardTwo: {
+    backgroundColor: 'green',
+  },
+  cardThree: {
+    backgroundColor: 'blue',
+  },
+});
